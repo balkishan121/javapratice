a7d5a944218a8dadf5447f3d1ce1171853cd93b9
Balkishan Padihar <98443289+balkishan121@users.noreply.github.com>
2024-01-08 19:43:46 +0530

    Update sumofelementusingvararg.java

--- sumofelementusingvararg.java
+++ sumofelementusingvararg.java
@@ -14,5 +14,8 @@ public class sumofelementusingvararg {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("The sum of all the element is: "+sum(10,20,30,40,50,60));
+         System.out.println("Maximum of the number is:"+sum(10,20,30,40,50,60,70,80,90));
+        System.out.println("maxmimum of the number is:"+sum(10,20,30,40,50,60,70));
+        System.out.println("Maximum of the number is:"+sum(10,20,30,40,50));
     }
 }
